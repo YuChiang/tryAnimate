27fff098d2b01ae013aaa1211a1c4551aab790c9
PapaChiang <annie84080@gmail.com>
2016-01-31 12:34:13 +0800

    button and image opacity hover

---
+++ js/demo.js
@@ -0,0 +1,10 @@
+//$(function(){
+//	// 預設的透明度值
+//	var _opacity = 0.7;
+// 
+//	// 在 .mask-div 中加入一個遮罩用的 .cover-mask, 並設定其透明度為預設的透明度值
+//	// 接著加入 .hover() 事件
+//	$('.mask-div').append('<div class="cover-mask"></div>').find('.cover-mask').css({
+//		opacity: _opacity
+//	});
+//});
